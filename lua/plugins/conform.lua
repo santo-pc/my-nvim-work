2b46f586f50c48e08bad3b7fef78342352ef9a5e
Santiago Palacio <s.palacio-caro@mytaxi.com>
2025-12-01 15:05:35 +0100

    gopls + autoformat fixes

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -37,6 +37,7 @@ return {
         rust = { 'rustfmt' },
         nix = { 'nixfmt' },
         elixir = { 'mix' },
+        go = { 'gofmt' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
