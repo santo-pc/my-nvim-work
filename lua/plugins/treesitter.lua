7d3ca3bc4399c3706d85a8abe7551ea8a4c2e2e9
Santiago Palacio Caro <sapalacio2@gmail.com>
2026-01-02 13:34:38 +0100

    add tree-sitter keymaps

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -25,6 +25,7 @@ return {
         scope_incremental = "<CR>",
         node_decremental = "<BS>",
       },
+    },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
