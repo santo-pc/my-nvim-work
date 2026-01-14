a69f54c05f48fae8d9672ddaa581e5009cfb8caf
Santiago Palacio Caro <sapalacio2@gmail.com>
2026-01-14 18:00:11 +0100

    vesper

--- lua/plugins/vesper.lua
+++ lua/plugins/vesper.lua
@@ -7,9 +7,9 @@ return {
         italics = {
           comments = true, -- Boolean: Italicizes comments
           keywords = true, -- Boolean: Italicizes keywords
-          functions = true, -- Boolean: Italicizes functions
-          strings = true, -- Boolean: Italicizes strings
-          variables = true, -- Boolean: Italicizes variables
+          functions = false, -- Boolean: Italicizes functions
+          strings = false, -- Boolean: Italicizes strings
+          variables = false, -- Boolean: Italicizes variables
         },
         overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
         palette_overrides = {},
